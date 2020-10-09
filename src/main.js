f1e8c4c3a411a0aa691131b697a7e723ce074c4f
Vincent Lejtzén <vincent@lejtzendesign.se>
2020-10-09 16:50:48 +0200

    Update meta

--- src/main.js
+++ src/main.js
@@ -3,50 +3,60 @@
 
 import DefaultLayout from '~/layouts/Default.vue'
 
-export default function (Vue, { router, head, isClient }) {
+export default function(Vue, { router, head, isClient }) {
     // Set default layout as a global component
     Vue.component('Layout', DefaultLayout)
 
     // Apply directive
-    Vue.directive('freeze', function (element) {
+    Vue.directive('freeze', function(element) {
         var canvas = document.createElement('canvas')
         var context = canvas.getContext('2d')
 
         canvas.setAttribute('class', element.classList.value)
 
-        element.onload = function () {
+        element.onload = function() {
             canvas.width = element.width
             canvas.height = element.height
             element.parentNode.replaceChild(canvas, element)
 
-            context.drawImage(element, 0, 0, element.width, element.height, 0, 0, canvas.width, canvas.height)
+            context.drawImage(
+                element,
+                0,
+                0,
+                element.width,
+                element.height,
+                0,
+                0,
+                canvas.width,
+                canvas.height,
+            )
         }
     })
 
     // Charset
     head.meta.push({
         name: 'charset',
-        content: 'UTF-8'
+        content: 'UTF-8',
     })
 
     // Viewport
     head.meta.push({
         name: 'viewport',
-        content: 'width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no, viewport-fit=cover'
+        content: 'width=device-width, initial-scale=1.0, viewport-fit=cover',
     })
 
     // Web app
     head.meta.push({
         name: 'apple-mobile-web-app-capable',
-        content: 'yes'
+        content: 'yes',
     })
     head.meta.push({
         name: 'apple-mobile-web-app-title',
-        content: 'Chat'
+        content: 'Ask',
     })
     head.meta.push({
         name: 'apple-mobile-web-app-status-bar-style',
-        content: 'black-translucent'
+        content: 'black-translucent',
     })
 }
 
