7bae719d89c0fb1cfe159b302785bdd009dc1071
Vincent Lejtzén <vincent@lejtzendesign.se>
2020-10-09 16:25:28 +0200

    Remove usage of Date.now()

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -168,7 +168,7 @@ export default {
                 id:
                     Math.random()
                         .toString(36)
-                        .substring(2) + Date.now().toString(36),
+                        .substring(2) + new Date().getTime().toString(36),
                 sender: sender,
                 type: type,
                 time: new Date(),
